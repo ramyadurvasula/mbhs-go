36c2a803f4e57d16ec7758cbc56467ab6dc20313
Helen <heroed1998@gmail.com>
2016-05-02 16:52:00 -0400

    5/2/16
    Merge imminent (I hope)

--- MBHSGo/app/src/main/java/com/example/ramya/mbhsgo/SlidingTabsBasicFragment.java
+++
@@ -1,9 +0,0 @@
-package com.example.ramya.mbhsgo;
-
-import android.support.v4.app.Fragment;
-
-/**
- * Created by Ramya on 3/22/2016.
- */
-public class SlidingTabsBasicFragment extends Fragment {
-}
